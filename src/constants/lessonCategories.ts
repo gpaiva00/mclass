3ac5f804aabde7a5cce38efb6b34d563e72009fa
Gabriel Paiva <gabriel.paiva0@icloud.com>
2025-02-01 23:39:12 -0300

    feat: add bus category

--- src/constants/lessonCategories.ts
+++ src/constants/lessonCategories.ts
@@ -3,7 +3,7 @@ import { Category } from "@/pages/Lessons";
 const lessonCategories: Category[] = [
   { id: "car", name: "Carro", slug: "car" },
   { id: "motorcycle", name: "Moto", slug: "motorcycle" },
+  { id: "bus", name: "Ônibus", slug: "bus" },
 ];
 
 export { lessonCategories };
-
